5bf7bec653375324af176fd1c3b9517d8afae7b0
Brooke Zwayer <brookezwayer@Olivias-MacBook-Pro.local>
2019-12-02 07:42:33 -0600

    added calendar and todos template views

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var app = express();
 require('dotenv').config();
 require('./config/passport');
 require('./config/database');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -28,6 +29,7 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,6 +39,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
